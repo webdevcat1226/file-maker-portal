52b878b61162cc4672654ce8fe9c5355793a84d0
Rusleo-max <leosatan1226@gmail.com>
2020-03-21 06:21:45 +1000

    feat: homepage table content added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  quotes = [
+    {number: 1},
+    {number: 2},
+    {number: 3},
+    {number: 4},
+    {number: 5},
+    {number: 6},
+    {number: 7},
+    {number: 8},
+    {number: 9},
+    {number: 10}
+  ];
+
+  submittedApplications = [
+    {number: 1},
+    {number: 2},
+    {number: 3},
+    {number: 4},
+    {number: 5},
+    {number: 6},
+    {number: 7},
+    {number: 8},
+    {number: 9},
+    {number: 10},
+    {number: 11},
+    {number: 12},
+    {number: 13},
+    {number: 14},
+    {number: 15},
+    {number: 16},
+    {number: 17},
+  ];
+
+  processedApplications = [
+    {number: 1},
+    {number: 2},
+    {number: 3},
+    {number: 4},
+    {number: 5},
+    {number: 6},
+    {number: 7},
+    {number: 8},
+    {number: 9},
+    {number: 10},
+    {number: 11},
+    {number: 12},
+    {number: 13},
+    {number: 14},
+    {number: 15},
+    {number: 16},
+    {number: 17},
+  ];
+
   constructor(
   ) { }
 
