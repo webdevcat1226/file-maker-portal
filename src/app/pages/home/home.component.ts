5ea82d1902e5d2ca884d33b635d3de78959d00ff
Rusleo-max <leosatan1226@gmail.com>
2020-03-23 01:26:35 +1000

    fix: homepage completed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,60 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   quotes = [
-    {number: 1},
-    {number: 2},
-    {number: 3},
-    {number: 4},
-    {number: 5},
-    {number: 6},
-    {number: 7},
-    {number: 8},
-    {number: 9},
-    {number: 10}
+    {availability: "N/A", type: "", status: "Unsubmitted", buttonStyle:"visibility: visible"},
+    {availability: "N/A", type: "", status: "Unsubmitted", buttonStyle:"visibility: visible"},
+    {availability: "N/A", type: "", status: "Unsubmitted", buttonStyle:"visibility: visible"},
+    {availability: "N/A", type: "Test", status: "Pending", buttonStyle:"visibility: hidden"},
+    {availability: "N/A", type: "", status: "Pending", buttonStyle:"visibility: hidden"},
   ];
 
   submittedApplications = [
-    {number: 1},
-    {number: 2},
-    {number: 3},
-    {number: 4},
-    {number: 5},
-    {number: 6},
-    {number: 7},
-    {number: 8},
-    {number: 9},
-    {number: 10},
-    {number: 11},
-    {number: 12},
-    {number: 13},
-    {number: 14},
-    {number: 15},
-    {number: 16},
-    {number: 17},
   ];
 
   processedApplications = [
-    {number: 1},
-    {number: 2},
-    {number: 3},
-    {number: 4},
-    {number: 5},
-    {number: 6},
-    {number: 7},
-    {number: 8},
-    {number: 9},
-    {number: 10},
-    {number: 11},
-    {number: 12},
-    {number: 13},
-    {number: 14},
-    {number: 15},
-    {number: 16},
-    {number: 17},
   ];
 
-  constructor(
-  ) { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
