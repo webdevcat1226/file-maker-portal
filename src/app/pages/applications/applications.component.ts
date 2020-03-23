5f18dbd9f1268413d7211c10855535990667fe9e
Rusleo-max <leosatan1226@gmail.com>
2020-03-23 22:35:55 +1000

    feat: applications page added

--- src/app/pages/applications/applications.component.ts
+++ src/app/pages/applications/applications.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationsComponent implements OnInit {
 
-  constructor() { }
+  applicationsInfo = [
+    {
+      name: "Test4",
+      address: "1 East Fen Common, Soham, CH75JH",
+      certificate: "No",
+      unresolvedAmendments: 1,
+      unresolvedHighlights: 14,
+      reviewedDocs: 0,
+      warranty: "Advantage",
+      inspector: "Will Jarvis"
+    },
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
