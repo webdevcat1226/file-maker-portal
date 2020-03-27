cdfea635006f9cdb3f7f673f44c50a9d4d3b198e
Rusleo-max <leosatan1226@gmail.com>
2020-03-27 23:10:25 +1000

    feat: auth background image changed and amendments page added

--- src/app/pages/applications/view-detail/view-detail-amendments/view-detail-amendments.component.ts
+++ src/app/pages/applications/view-detail/view-detail-amendments/view-detail-amendments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApplicationService } from '../../../../core/services/application.service';
 import { ActivatedRoute } from '@angular/router';
+import * as Faker from 'faker';
 
 @Component({
   selector: 'app-view-detail-amendments',
@@ -11,6 +12,73 @@ export class ViewDetailAmendmentsComponent implements OnInit {
 
   id: string;
   data: any;
+  amendments = [
+    {
+      number: '1',
+      character: 'A',
+      serial: '74',
+      content: 'Please provide Engineers design and calculations, upon receipt we will either check in house or pass to our checking engineer. Therefore further comments may follow.',
+      isComplete: 'Yes',
+      color: 'limegreen'
+    },
+    {
+      number: '1',
+      character: 'A',
+      serial: '75',
+      content: 'We appear to have received the structural design but in order to  carry out our structural check we also require the accompanying calculations.',
+      isComplete: 'Yes',
+      color: 'limegreen'
+    },
+    {
+      number: '2',
+      character: 'A',
+      serial: '76',
+      content: 'Due to the recent adverse publicity concerning breathable felts, it is suggested that additional ventilation is provided to avoid the risk of condensation in the roof.',
+      isComplete: 'Yes',
+      color: 'limegreen'
+    },
+    {
+      number: '3',
+      character: 'A',
+      serial: '77',
+      content: 'We appear to have received the structural design but in order to  carry out our structural check we also require the accompanying calculations.',
+      isComplete: 'No',
+      color: 'red'
+    },
+    {
+      number: '1',
+      character: 'A',
+      serial: '74',
+      content: 'Please provide Engineers design and calculations, upon receipt we will either check in house or pass to our checking engineer. Therefore further comments may follow.',
+      isComplete: 'Yes',
+      color: 'limegreen'
+    },
+    {
+      number: '1',
+      character: 'A',
+      serial: '75',
+      content: 'We appear to have received the structural design but in order to  carry out our structural check we also require the accompanying calculations.',
+      isComplete: 'Yes',
+      color: 'limegreen'
+    },
+    {
+      number: '2',
+      character: 'A',
+      serial: '76',
+      content: 'Due to the recent adverse publicity concerning breathable felts, it is suggested that additional ventilation is provided to avoid the risk of condensation in the roof.',
+      isComplete: 'Yes',
+      color: 'limegreen'
+    },
+    {
+      number: '3',
+      character: 'A',
+      serial: '77',
+      content: 'We appear to have received the structural design but in order to  carry out our structural check we also require the accompanying calculations.',
+      isComplete: 'No',
+      color: 'red'
+    },
+
+  ];
 
   constructor(
     private applicationService: ApplicationService,
