2b822e7c6e1155c64e45476c82cfab8fa4c20476
Rusleo-max <leosatan1226@gmail.com>
2020-04-02 23:53:18 +1000

    feat: small modifications in style, and two inspector contact info box added to view-detail-home component.

--- src/app/pages/applications/view-detail/view-detail-home/view-detail-home.component.ts
+++ src/app/pages/applications/view-detail/view-detail-home/view-detail-home.component.ts
@@ -12,6 +12,9 @@ export class ViewDetailHomeComponent implements OnInit {
   id: string;
   data: any;
 
+  userPhoto1: any = '/assets/background-images/cute rat.jpg';
+  userPhoto2: any = '/assets/background-images/cute rat.jpg';
+
   constructor(
     private applicationService: ApplicationService,
     private route: ActivatedRoute
@@ -20,5 +23,26 @@ export class ViewDetailHomeComponent implements OnInit {
   ngOnInit(): void {
     this.id =  this.route.parent.snapshot.params.id;
     this.data = this.applicationService.findById(this.id);
+    console.log(this.data);
+  }
+
+  handleFileInput1(files) {
+    if (FileReader && files && files.length) {
+      const fr = new FileReader();
+      fr.onload = () => {
+        this.userPhoto1 = fr.result;
+      };
+      fr.readAsDataURL(files[0]);
+    }
+  }
+
+  handleFileInput2(files) {
+    if (FileReader && files && files.length) {
+      const fr = new FileReader();
+      fr.onload = () => {
+        this.userPhoto2 = fr.result;
+      };
+      fr.readAsDataURL(files[0]);
+    }
   }
 }
